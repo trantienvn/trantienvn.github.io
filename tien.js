d693ef467ec4c3d4c0183f2349cddd87bf201c3e
TranTien <tientn2005@gmail.com>
2024-10-18 10:55:29 +0700

    update

---
+++ tien.js
@@ -0,0 +1,11 @@
+const shopee = [
+    "https://s.shopee.vn/6ASxcnDiVh",
+    "https://s.shopee.vn/6V5tpQ6Yg0",
+    "https://s.shopee.vn/702AQNvGKB",
+    "https://s.shopee.vn/VoggSez1N",
+    "https://s.shopee.vn/9zflzwvpRs",
+    "https://s.shopee.vn/AUc2auNupF",
+    "https://s.shopee.vn/3q58efqfj6",
+    "https://s.shopee.vn/AKIcOdS8qw",
+    "https://s.shopee.vn/9f2vbRTOHY"
+]
